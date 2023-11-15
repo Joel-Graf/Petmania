e266b089bac55a65f3428a680d92af3652360b63
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2023-11-15 17:19:05 -0300

    up

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: [
+    'vuetify'
+  ],
+  chainWebpack: config => {
+    // Adicione ou ajuste as configurações do Webpack usando chainWebpack
+    // Exemplo: configurando uma regra para vue-loader
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        // Modifique as opções do vue-loader aqui, se necessário
+        return options;
+      });
+  }
+};
